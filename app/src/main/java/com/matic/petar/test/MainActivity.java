13f850e61528d0d3fe2703d4688a21393ea52157
petar55 <matic.petar91@gmail.com>
2016-11-11 11:11:48 +0100

    neke izmjene

--- app/src/main/java/com/matic/petar/test/MainActivity.java
+++ app/src/main/java/com/matic/petar/test/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //neki komentar, test 123
+        //neki komentar, test 123 ...
     }
 }
